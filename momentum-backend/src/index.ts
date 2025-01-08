04fc5a24fc58ce442f7e03032b691bd3171849c2
rutgerswirski <rutgerswirski.johan@gmail.com>
2025-01-08 17:05:30 +0100

    succcessful connection to mongodb backend

--- momentum-backend/src/index.ts
+++ momentum-backend/src/index.ts
@@ -1,5 +1,9 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 import express, { Request, Response } from "express";
 import exampleRoute from "./routes/example";
+
+dotenv.config();
 
 const app = express();
 const port = 5000;
@@ -11,6 +15,16 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api", exampleRoute);
+// app.use("api/auth", authRoutes);
+// dashboard
+// app.use("api/dashboard", dashboardRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI || "mongodb://localhost:27017")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
